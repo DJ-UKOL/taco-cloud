f64381e35b6a70b747277f1d3d04b78b333a406a
DJ-UKOL <dj-ukol@yandex.ru>
2022-12-15 11:51:37 +0500

    Initial

---
+++ taco-cloud/src/main/java/ru/dinerik/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package ru.dinerik.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Основной класс который запускает проект
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
