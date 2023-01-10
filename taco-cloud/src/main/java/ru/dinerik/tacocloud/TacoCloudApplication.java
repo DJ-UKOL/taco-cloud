6b156dd8590cad74a8ea41ef5cf217b2f1b85727
DJ-UKOL <dj-ukol@yandex.ru>
2023-01-10 16:39:58 +0500

    Ex_7.1 (Create Controllers RESTFul)

--- taco-cloud/src/main/java/ru/dinerik/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/ru/dinerik/tacocloud/TacoCloudApplication.java
@@ -1,42 +1,13 @@
 package ru.dinerik.tacocloud;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import ru.dinerik.tacocloud.Ingredient.Type;
-import ru.dinerik.tacocloud.data.IngredientRepository;
 
 // Основной класс который запускает проект
 @SpringBootApplication			// (@SpringBootConfiguration, @EnableAutoConfiguration, @ComponentScan)
-public class TacoCloudApplication implements WebMvcConfigurer {
+public class TacoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/login");
-	}
-
-	@Bean
-	public CommandLineRunner dataLoader(IngredientRepository repo) {
-		return args -> {
-			repo.deleteAll(); // TODO: Quick hack to avoid tests from stepping on each other with constraint violations
-			repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-			repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-			repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-			repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-			repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-			repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-			repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-			repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-			repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-			repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-		};
 	}
 }
