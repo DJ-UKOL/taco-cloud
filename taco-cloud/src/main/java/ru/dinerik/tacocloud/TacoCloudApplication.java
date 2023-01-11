a39dd9c2f9f5a5a53cfd8526d4efa332ecefd0a3
DJ-UKOL <dj-ukol@yandex.ru>
2023-01-11 10:28:03 +0500

    Ex_7.2 (Spring Data REST)

--- taco-cloud/src/main/java/ru/dinerik/tacocloud/TacoCloudApplication.java
+++ taco-cloud/src/main/java/ru/dinerik/tacocloud/TacoCloudApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
+		System.out.println(1 + '0');
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 }
